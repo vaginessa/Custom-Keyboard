3b937c7bf56be2324e2eb31dea638f40e0998d41
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2017-05-06 13:39:52 +0530

    first commit

---
+++ app/src/main/java/com/developers/simplekeyboard/SimpleIME.java
@@ -0,0 +1,58 @@
+package com.developers.simplekeyboard;
+
+import android.app.Service;
+import android.content.Intent;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.os.IBinder;
+
+public class SimpleIME extends InputMethodService implements KeyboardView.OnKeyboardActionListener{
+    private KeyboardView kv;
+    private Keyboard keyboard;
+
+    private boolean caps = false;
+    public SimpleIME() {
+    }
+
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int i, int[] ints) {
+
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
